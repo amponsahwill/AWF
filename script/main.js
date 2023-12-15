63f1f201ca37931d68e9b5936bb7507ff8349ee8
amponsahwill <willamponsah01@gmail.com>
2023-12-15 11:38:51 -0500

    added a new site

--- script/main.js
+++ script/main.js
@@ -1,6 +1,15 @@
-// const form = document.getElementById("myForm");
-// form.addEventListener("submit", (event) => {
-//   event.preventDefault();
-//   form.reset();
-// });
+$( document ).ready(function() {
+	//Waypoints
+	$('.under-inner').each(function(){
+		var $this = $(this);
+		$this.waypoint(function(direction) {
+			if (direction === 'down') {	
+			  $this.children(".fixed").addClass('locked');
+			}else{
+			  $this.children(".fixed").removeClass('locked');
+			}
+		}, { offset: '60%' });
+	});
+	//End Waypoints
+});
 
